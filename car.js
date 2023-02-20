0d9dc16e5bf30c104040f92fbc5b6a4531e8e49d
Julian Rosero <julian-1911@hotmail.com>
2023-02-19 21:19:42 -0500

    numberDay and timeVerfication

--- car.js
+++ car.js
@@ -9,13 +9,31 @@ const lastDigit = (plate) => {
   const plateSplit = plate.split("");
   return parseInt(plateSplit[plateSplit.length - 1]);
 };
-const dayVerification = (date, plate) => {
-  numberDay = parseInt(new Date(date).getDay()) + 1;
-  console.log(numberDay);
-
+const numberDay = (date) => {
+  return parseInt(new Date(date).getDay()) + 1;
+};
+const dayVerification = (plate, date) => {
   const nowDay = Object.keys(plateData).find(
-    (item) => parseInt(item) === numberDay
+    (item) => parseInt(item) === numberDay(date)
   );
 
   return !plateData[nowDay]?.includes(lastDigit(plate));
+};
+const timeVerification = (time) => {
+  const morningHours = {
+    start: new Date().setHours(7, 0),
+    end: new Date().setHours(9, 30),
+  };
+  const eveningHours = {
+    start: new Date().setHours(16, 0),
+    end: new Date().setHours(19, 30),
+  };
+  const [hours, minutes] = time.split(":").map(Number);
+  const nowDate = new Date().setHours(hours, minutes);
+  if (
+    (nowDate >= morningHours.start && nowDate <= morningHours.end) ||
+    (nowDate >= eveningHours.start && nowDate <= eveningHours.end)
+  ) {
+    return false;
+  } else return true;
 };
